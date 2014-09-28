8b24cf0717b357e8e61b08e18184aaeccc47b05f
James E. Marca <james@activimetrics.com>
2014-09-28 12:58:30 -0700

    basic web app via ring
    does nothing interactive, but I can dump a parsed string to the screen
    at the route /parse
    
    modified:   .gitignore
    new file:   public/index.html
    new file:   public/project.clj
    new file:   public/src-clj/example/parse.clj
    new file:   public/src-clj/example/routes.clj
    new file:   public/src-clj/example/views.clj
    new file:   public/src-cljs/async_tut1/core.cljs

---
+++ public/src-clj/example/routes.clj
@@ -0,0 +1,18 @@
+(ns example.routes
+  (:use compojure.core
+        example.views
+        example.parse
+        [hiccup.middleware :only (wrap-base-url)])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
+
+(defroutes main-routes
+  (GET "/" [] (index-page))
+  (GET "/parse" [] (parse-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (-> (handler/site main-routes)
+      (wrap-base-url)))
