49951cb122f44973830502fa956c4e1d8358916b
James E. Marca <james@activimetrics.com>
2014-09-28 01:36:35 -0700

    better system for stitch repeats
    still a hassle brewing with the whole "twice" stuff, but not as bad as
    before.  Punting on "three times" going for "3 times"
    
    possibly use "X" or "times"?

--- resource/garterstitch.cljs
+++ resource/garterstitch.cljs
@@ -8,10 +8,13 @@
     linenumber = R <whitespace>? number
     <R> = 'r' | 'R' | 'row' | 'Row'
     <token> = K | YO | K2tog
-    K : 'k' number?
-    YO : 'yo' | 'yo' <whitespace> 'twice'
-    K2tog : 'k2tog'+
-    number : #'[0-9]+'
+    K : 'k' (<whitespace>? repeat)?
+    YO : 'yo'  (<whitespace> repeat)?
+    repeat: (number | 1 | 2 | number <whitespace 'times'>)+
+    1 = <'once'>
+    2 = <'twice'>
+    K2tog : 'k2tog' (<whitespace> repeat)?
+    <number> : #'[0-9]+'
     whitespace = #'\\s+'
 "
    ))
@@ -20,4 +23,5 @@
 (insta/parses knit-and-yarnover "Row 1 k3 yo k k2tog k k2tog k yo k3
                                  R2: k
                                  R3: k3 k2tog k yo k yo twice k k2tog k3
-                                 R4: k")
+                                 R4: k
+                                 R3: k3 k2tog twice k yo 3 times k yo twice k k2tog k")
